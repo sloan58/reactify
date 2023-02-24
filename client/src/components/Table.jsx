9316dc9873de67d004a734b8f73eba1b8dccf11d
Marty Sloan <marty@Carlas-MacBook-Pro.local>
2023-02-24 04:42:08 -0500

    Add regular Table to dashboard

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -6,7 +6,7 @@ const Table = () => {
     useEffect(() => {
         apiClient
             .get("/api/users")
-            .then((users) => setUsers(users.data))
+            .then((users) => setUsers(users.data.data))
             .catch((err) => console.error(err));
     }, []);
     return (
@@ -15,53 +15,27 @@ const Table = () => {
                 <thead className="border-b text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                            Product name
+                            Name
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Color
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Category
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Price
+                            Email
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th
-                            scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                        >
-                            Apple MacBook Pro 17"
-                        </th>
-                        <td className="px-6 py-4">Silver</td>
-                        <td className="px-6 py-4">Laptop</td>
-                        <td className="px-6 py-4">$2999</td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th
-                            scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                        >
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="px-6 py-4">White</td>
-                        <td className="px-6 py-4">Laptop PC</td>
-                        <td className="px-6 py-4">$1999</td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th
-                            scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                        >
-                            Magic Mouse 2
-                        </th>
-                        <td className="px-6 py-4">Black</td>
-                        <td className="px-6 py-4">Accessories</td>
-                        <td className="px-6 py-4">$99</td>
-                    </tr>
+                    {users.map((user) => {
+                        return (
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    {user.name}
+                                </th>
+                                <td className="px-6 py-4">{user.email}</td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import AuthLayout from "../layout/AuthLayout.jsx";
 import TanTable from "../components/TanTable.jsx";
+import Table from "../components/Table.jsx";
 
 const Dashboard = () => {
     return (
         <AuthLayout>
             <div className="w-2/3 mx-auto">
                 <TanTable />
+                <Table />
             </div>
         </AuthLayout>
     );
