3c645c113f9f64cb4bd0703fea3ce51d9a9246d7
Marty Sloan <marty@Carlas-MacBook-Pro.local>
2023-02-24 19:51:16 -0500

    Extract Tanstack Table pagination to component

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,45 +1,109 @@
-import { useEffect, useState } from "react";
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import Paginator from "./Paginator.jsx";
+import { useEffect, useMemo, useState } from "react";
 import apiClient from "../utils/apiClient.js";
 
-const Table = () => {
-    const [users, setUsers] = useState([]);
+function Table({source}) {
+    
+    const columnHelper = createColumnHelper();
+
+    const columns = [
+        columnHelper.accessor("name", {
+            header: "Name",
+            cell: (info) => (
+                <span className="font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {info.getValue()}
+            </span>
+            ),
+        }),
+        columnHelper.accessor((row) => row.email, {
+            header: "Email",
+        }),
+    ];
+
+    const [data, setData] = useState(() => []);
+
+    const [{ pageIndex, pageSize }, setPagination] = useState({
+        pageIndex: 0,
+        pageSize: 10,
+    });
+
     useEffect(() => {
         apiClient
-            .get("/api/users")
-            .then((users) => setUsers(users.data.data))
-            .catch((err) => console.error(err));
-    }, []);
+            .get(`${source}?page=${pageIndex + 1}&pageSize=${pageSize}`)
+            .then((res) => {
+                setData(res.data);
+            });
+    }, [pageIndex, pageSize]);
+
+    const defaultData = useMemo(() => [], []);
+
+    const pagination = useMemo(
+        () => ({
+            pageIndex,
+            pageSize,
+        }),
+        [pageIndex, pageSize]
+    );
+
+    const table = useReactTable({
+        data: data.data ?? defaultData,
+        columns,
+        pageCount: data?.last_page ?? -1,
+        state: {
+            pagination,
+        },
+        onPaginationChange: setPagination,
+        getCoreRowModel: getCoreRowModel(),
+        manualPagination: true,
+        debugTable: true,
+    });
+
     return (
         <div className="relative overflow-x-auto border-2 p-4 rounded">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="border-b text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Email
-                        </th>
-                    </tr>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th key={header.id} className="px-6 py-3">
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                              header.column.columnDef.header,
+                                              header.getContext()
+                                          )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
                 </thead>
                 <tbody>
-                    {users.map((user) => {
-                        return (
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th
-                                    scope="row"
-                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                >
-                                    {user.name}
-                                </th>
-                                <td className="px-6 py-4">{user.email}</td>
-                            </tr>
-                        );
-                    })}
+                    {table.getRowModel().rows.map((row) => (
+                        <tr
+                            key={row.id}
+                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                        >
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id} className="px-6 py-4">
+                                    {flexRender(
+                                        cell.column.columnDef.cell,
+                                        cell.getContext()
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+            <Paginator table={table} pagination={pagination} />
         </div>
     );
-};
+}
 
 export default Table;
